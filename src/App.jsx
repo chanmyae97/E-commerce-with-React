328f19ee27c34c0c02918427dadc45af96aaed0e
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-01-08 16:50:55 +0700

    Creating Nav-bar and Products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Nav from "./Navigation/Nav";
+import Products from "./Products/Products.jsx";
+import Recommended from "./Recommended/Recommended.jsx";
+
 function App() {
-  return <h1> Hello World</h1>;
+  return (
+    <>
+      <Nav />
+      <Products />
+      <Recommended />
+    </>
+  );
 }
 
 export default App;
