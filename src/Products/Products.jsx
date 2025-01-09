85a26ac36a550abc4958084c47a2db4f64e72849
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-01-09 16:25:41 +0700

    Creating Sidebar components

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,161 +1,21 @@
 import { AiFillStar } from "react-icons/ai";
 import "./Products.css";
 import { BsFillBagHeartFill } from "react-icons/bs";
+import Card from "../conponents/Card";
 
 const Products = () => {
   return (
     <>
       <section className="card-container">
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-review">
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-              <div className="bag">
-                <BsFillBagHeartFill className="bag-icon" />
-              </div>
-            </section>
-          </div>
-        </section>
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-review">
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-              <div className="bag">
-                <BsFillBagHeartFill className="bag-icon" />
-              </div>
-            </section>
-          </div>
-        </section>
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-review">
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-              <div className="bag">
-                <BsFillBagHeartFill className="bag-icon" />
-              </div>
-            </section>
-          </div>
-        </section>
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-review">
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-              <div className="bag">
-                <BsFillBagHeartFill className="bag-icon" />
-              </div>
-            </section>
-          </div>
-        </section>
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-review">
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-              <div className="bag">
-                <BsFillBagHeartFill className="bag-icon" />
-              </div>
-            </section>
-          </div>
-        </section>
-        <section className="card">
-          <img
-            src="https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg"
-            alt="Shoe"
-            className="card-img"
-          />
-          <div className="card-details">
-            <h3 className="card-title">Shoe</h3>
-            <section className="card-review">
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <AiFillStar className="ratings-start" />
-              <span className="total-reviews">4</span>
-            </section>
-            <section className="card-price">
-              <div className="price">
-                <del>$300</del>$200
-              </div>
-              <div className="bag">
-                <BsFillBagHeartFill className="bag-icon" />
-              </div>
-            </section>
-          </div>
-        </section>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </section>
     </>
   );
--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,7 +1,13 @@
+import Input from "../../conponents/Input";
 import "./Colors.css";
 
 const Colors = () => {
-  return <div>Colors</div>;
+  return (
+    <div>
+      <h2 className="sidebar-title color-title">Colors</h2>
+      <Input />
+    </div>
+  );
 };
 
 export default Colors;
